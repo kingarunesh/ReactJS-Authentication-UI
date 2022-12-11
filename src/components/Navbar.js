b2aad8f7ddad14ea417f78bf9787db1c6f60dbef
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-12-11 14:27:07 +0530

    3.6 : login redirect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,32 +3,32 @@ import { Box, AppBar, Toolbar, Typography, Button } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  const activePage = ({ isActive }) => {
-    return { backgroundColor: isActive ? "#004182" : "" };
-  };
+    const activePage = ({ isActive }) => {
+        return { backgroundColor: isActive ? "#004182" : "" };
+    };
 
-  return (
-    <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" color="primary">
-          <Toolbar>
-            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-              Classimax
-            </Typography>
-            <Button component={NavLink} to="/" sx={{ color: "white", textTransform: "none", fontSize: "1rem" }} style={activePage}>
-              Home
-            </Button>
-            <Button component={NavLink} to="/contact" sx={{ color: "white", textTransform: "none", fontSize: "1rem" }} style={activePage}>
-              Contact
-            </Button>
-            <Button component={NavLink} to="/login" sx={{ color: "white", textTransform: "none", fontSize: "1rem" }} style={activePage}>
-              Login / Register
-            </Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </>
-  );
+    return (
+        <>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static" color="primary">
+                    <Toolbar>
+                        <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+                            Classimax
+                        </Typography>
+                        <Button component={NavLink} to="/" sx={{ color: "white", textTransform: "none", fontSize: "1rem" }} style={activePage}>
+                            Home
+                        </Button>
+                        <Button component={NavLink} to="/contact" sx={{ color: "white", textTransform: "none", fontSize: "1rem" }} style={activePage}>
+                            Contact
+                        </Button>
+                        <Button component={NavLink} to="/login" sx={{ color: "white", textTransform: "none", fontSize: "1rem" }} style={activePage}>
+                            Login
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </>
+    );
 };
 
 export default Navbar;
--- src/components/pages/auth/LoginForm.js
+++ src/components/pages/auth/LoginForm.js
@@ -1,53 +1,59 @@
 import React, { useState } from "react";
 import { TextField, Button, Box, Alert } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
-  const [error, setError] = useState({
-    status: false,
-    message: "",
-    type: "",
-  });
+    const [error, setError] = useState({
+        status: false,
+        message: "",
+        type: "",
+    });
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+    const navigate = useNavigate();
 
-    const data = new FormData(event.currentTarget);
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
-    const accuretData = {
-      email: data.get("email"),
-      password: data.get("password"),
+        const data = new FormData(event.currentTarget);
+
+        const accuretData = {
+            email: data.get("email"),
+            password: data.get("password"),
+        };
+
+        if (accuretData.email && accuretData.password) {
+            console.log(accuretData);
+
+            setError({ status: true, message: "Login Success", type: "success" });
+
+            document.getElementById("login-form").reset();
+
+            navigate("/");
+        } else {
+            setError({ status: true, message: "All fields are require", type: "error" });
+        }
     };
 
-    if (accuretData.email && accuretData.password) {
-      console.log(accuretData);
-      setError({ status: true, message: "Login Success", type: "success" });
-      document.getElementById("login-form").reset();
-    } else {
-      setError({ status: true, message: "All fields are require", type: "error" });
-    }
-  };
+    return (
+        <>
+            <Box component="form" noValidate sx={{ m: 1 }} id="login-form" onSubmit={handleSubmit}>
+                {error.status && (
+                    <Alert severity={error.type} sx={{ mt: 2 }}>
+                        {error.message}
+                    </Alert>
+                )}
+                <TextField margin="normal" required fullWidth id="email" name="email" label="Email Address" />
+                <TextField margin="normal" required fullWidth id="password" name="password" label="Password" type="password" />
 
-  return (
-    <>
-      <Box component="form" noValidate sx={{ m: 1 }} id="login-form" onSubmit={handleSubmit}>
-        {error.status && (
-          <Alert severity={error.type} sx={{ mt: 2 }}>
-            {error.message}
-          </Alert>
-        )}
-        <TextField margin="normal" required fullWidth id="email" name="email" label="Email Address" />
-        <TextField margin="normal" required fullWidth id="password" name="password" label="Password" type="password" />
-
-        <Box textAlign="center">
-          <Button type="submit" variant="contained" sx={{ mt: 2, mb: 2, px: 2 }}>
-            Login
-          </Button>
-        </Box>
-        <NavLink to="/">Forgot Password ?</NavLink>
-      </Box>
-    </>
-  );
+                <Box textAlign="center">
+                    <Button type="submit" variant="contained" sx={{ mt: 2, mb: 2, px: 2 }}>
+                        Login
+                    </Button>
+                </Box>
+                <NavLink to="/">Forgot Password ?</NavLink>
+            </Box>
+        </>
+    );
 };
 
 export default LoginForm;
