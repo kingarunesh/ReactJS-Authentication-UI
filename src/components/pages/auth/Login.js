49b0ab6d180416620259ed8120e77ff7602a8c6e
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-12-11 09:35:36 +0530

    3.1 : image added on login page

--- src/components/pages/auth/Login.js
+++ src/components/pages/auth/Login.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Grid } from "@mui/material";
+import Pic1 from "../../../images/form.png";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <Grid container sx={{ height: "90vh" }}>
+        <Grid
+          item
+          lg={7}
+          sm={5}
+          sx={{ backgroundImage: `url(${Pic1})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center" }}
+        ></Grid>
+
+        <Grid item lg={5} sm={7} sx={{}}></Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default Login;
