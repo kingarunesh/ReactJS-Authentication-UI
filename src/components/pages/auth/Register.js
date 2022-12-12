275de532dac7450443dfc9797e35a50a8057823f
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-12-12 08:40:05 +0530

    4.2 : registations form completed

--- src/components/pages/auth/Register.js
+++ src/components/pages/auth/Register.js
@@ -1,9 +1,77 @@
 import React, { useState } from "react";
+import { TextField, Button, Box, Alert, FormControlLabel, Checkbox } from "@mui/material";
+import { Form, NavLink, useNavigate } from "react-router-dom";
 
 const Register = () => {
+    const [error, setError] = useState({
+        status: false,
+        message: "",
+        type: "",
+    });
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = new FormData(event.currentTarget);
+
+        const accuretData = {
+            name: data.get("name"),
+            email: data.get("email"),
+            password: data.get("password"),
+            confirm_password: data.get("confirm_password"),
+            tc: data.get("tc"),
+        };
+
+        if (accuretData.name && accuretData.email && accuretData.password && accuretData.confirm_password && accuretData.tc !== null) {
+            if (accuretData.password === accuretData.confirm_password) {
+                console.log(accuretData);
+                setError({ status: true, message: "Registations Success", type: "success" });
+                document.getElementById("register-form").reset();
+            } else {
+                setError({ status: true, message: "Didn't match password and confirm password", type: "error" });
+            }
+
+            navigate("/");
+        } else {
+            setError({ status: true, message: "All fields are require", type: "error" });
+        }
+    };
+
     return (
         <>
-            <h1>Register</h1>
+            <Box component="form" noValidate sx={{ m: 1 }} id="register-form" onSubmit={handleSubmit}>
+                {error.status && (
+                    <Alert severity={error.type} sx={{ mt: 2 }}>
+                        {error.message}
+                    </Alert>
+                )}
+
+                <TextField margin="normal" required fullWidth id="name" name="name" label="Full Name" />
+
+                <TextField margin="normal" required fullWidth id="email" name="email" label="Email Address" />
+
+                <TextField margin="normal" required fullWidth id="password" name="password" label="Password" type="password" />
+
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="confirm_password"
+                    name="confirm_password"
+                    label="Confirm Password"
+                    type="password"
+                />
+
+                <FormControlLabel control={<Checkbox value="agree" color="primary" name="tc" id="tc" />} label="I agree to terms and conditions." />
+
+                <Box textAlign="center">
+                    <Button type="submit" variant="contained" sx={{ mt: 2, mb: 2, px: 2 }}>
+                        Join
+                    </Button>
+                </Box>
+            </Box>
         </>
     );
 };
